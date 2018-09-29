96497ee4aea600b037497fb3a2fbc23a1ae0c248
justworx <justworx@protonmail.com>
2018-09-29 17:03:36 -0500

    add base-64, -32, and -16 translation

--- net/irc/plugin/calc.py
+++ net/irc/plugin/calc.py
@@ -6,7 +6,7 @@
 
 from . import *
 from ....util.matheval import *
-
+from ....util.compenc import *
 
 class IRCCalc(IRCPlugin):
 	"""Useful commands for controlling the bot via privmsg/notify."""
@@ -18,17 +18,43 @@ class IRCCalc(IRCPlugin):
 		
 		# only handles PRIVMSG 	and NOTICE events
 		if not (e.irccmd in ["PRIVMSG","NOTICE"]):
+			return 
+
+		cmd = e.argvl[0]
+		if not cmd.strip():
 			return
 		
 		try:
-			cmd = e.argvl[0]
+			args = " ".join(e.argvl[1:])
 			if cmd in ['calc', 'calculate']:
-				self.reply(e, str(matheval(" ".join(e.argvl[1:]))))
+				self.reply(e, str(matheval(args)))
+			
+			else:
+				enc = self.bot.encoding
+				try:
+					spx = e.text.split(' ', 1) # get all but the first word
+					bts = spx[1].encode(enc)   # encode it to bytes
+					if cmd == 'b64':
+						self.reply(e, b64.encode(bts).decode(enc))
+					elif cmd == 'b64d':
+						self.reply(e, b64.decode(bts).decode(enc))
+					elif cmd == 'b32':
+						self.reply(e, b32.encode(bts).decode(enc))
+					elif cmd == 'b32d':
+						self.reply(e, b32.decode(bts).decode(enc))
+					elif cmd == 'b16':
+						self.reply(e, b16.encode(bts).decode(enc))
+					elif cmd == 'b16d':
+						self.reply(e, b16.decode(bts).decode(enc))
+				except IndexError:
+					err = "%s command requires arguments. Eg, `%s some text`"
+					self.reply(e, err % (cmd, cmd))
+				
 		except Exception as ex:
 			typ = str(type(ex))
 			err = str(ex)
 			irc.debug("command plugin error", typ, err)
 			
 			msg = "%s: %s" % (typ, err)
-			self.reply(msg)
+			self.reply(e, msg)
 		
