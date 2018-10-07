42495d8d46fc1c3fe9d92951227c671cdcfbd146
justworx <justworx@protonmail.com>
2018-10-07 03:44:42 -0500

    add dir plugin (and console config)

---
+++ app/plugin/dir.py
@@ -0,0 +1,38 @@
+#
+# Copyright 2018 justworx
+# This file is part of the trix project, distributed under
+# the terms of the GNU Affero General Public License.
+#
+
+
+from trix.app.plugin import *
+
+
+class Dir(Plugin):
+	"""Useful commands for controlling the bot via privmsg/notify."""
+	
+	COMMANDS = {
+		'ls' : lambda d,*a: d.ls(*a),
+		'cd' : lambda d,*a: d.cd(*a)		
+	}
+	
+	def __init__(self, pname, owner, config, **k):
+		Plugin.__init__(self, pname, owner, config, **k)
+		
+		p = config.get('path', '.')
+		self.__dir = trix.path(p).dir()
+	
+	
+	#
+	# HANDLE
+	#	
+	def handle(self, e):
+	
+		cmd = e.argvl[0]
+		if cmd in self.COMMANDS:
+			x = self.COMMANDS[cmd]
+			a = e.argv[1:]
+			r = x(self.__dir, *a)
+			self.reply(e, ", ".join(r))
+		
+		
