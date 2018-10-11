8cde13dab53317a1ba063b392851194cc9e0e98f
justworx <justworx@protonmail.com>
2018-10-11 12:37:29 -0500

    x.console updates

--- x/console/__init__.py
+++ x/console/__init__.py
@@ -24,14 +24,13 @@ class Console(EncodingHelper):
 	"""Base class for an interactive terminal-based user interface."""
 	
 	DefLang = 'en'
-	DefConf = "app/console/config/%s.conf"
+	DefConf = "x/console/config/%s.conf"  # REM: Change x to app!
 	
 	def __init__(self, config=None, **k):
 		"""
 		Pass config dict keys:
 		 * title    : The console's title, "Console".
 		 * about    : A string with any message/description/info
-		 * prompt   : If set, replaces the default command prompt
 		 * messages : A dict containing message key/value pairs
 		 * help     : A dict containing keywords/help messages
 		 * closing  : Message to print when exiting console
@@ -40,15 +39,12 @@ class Console(EncodingHelper):
 		COMMANDS:
 		 * help : print help on the specified topic
 		 * exit : exit the console (returning to previous activity)
-		
-		EXTEND CONSOLE:
-		To extend the console class, add commands (and their 'help'). 
 		"""
-		
+		#
+		# ident
+		#
 		id_mod = self.__module__.split('.')[-2]
-		id_type = type(self).__name__
-		id_user = getuser()
-		self.ident = "%s@%s/%s" % (id_user, id_mod, id_type)
+		self.__ident_prefix = "%s@%s" % (getuser(), id_mod)
 		
 		#
 		# If no config is given, use a default config path. Subclasses
@@ -58,38 +54,61 @@ class Console(EncodingHelper):
 		try:
 			# if config is a dict, update with kwargs it and continue
 			config.update(**k)
-			trix.display(config)
 		except AttributeError:
-			# ...otherwise, config must be the inner file path to a config
-			# file such as 'apps/config/en.conf'.
-			confile = trix.innerfpath(config)
-			jconf   = JConfig(confile)
+			#
+			# otherwise config is given by the `config` argument or by 
+			# a 'config' or 'nconfig' kwargs (which take precedence over 
+			# the `config` argument).
+			#
 			
-			# save the config
-			config  = jconf.obj
+			# at this point, config is a file path
+			if "nconfig" in k:
+				config = trix.nconfig(config['nconfig'], **k)
+			elif "config" in k:
+				config = trix.config(config['nconfig'], **k)
+			else:
+				config = trix.nconfig(config, **k)
+			
+		# -- now config is a dict --
 		
 		EncodingHelper.__init__(self, config)
+		
 		
 		# debugging - store config
 		self.__config = config
 
-		self.__prompt = self.ident
 		
 		#
 		# set member variables from config
 		#
-		self.__title    = config.get('title', 'Console')
-		self.__about    = config.get('about', 'About')
-		self.__help     = config.get('help', {})
-		self.__closing  = config.get('closing', 'Closing')
-		self.__messages = config.get('messages', {})
+		self.__title    = config.get('title'   , 'Console')
+		self.__about    = config.get('about'   , 'Hello.' )
+		self.__closing  = config.get('closing' , 'Closing')
+		self.__help     = config.get('help'    , {}       )
+		self.__messages = config.get('messages', {}       )
 		
+		# exerimental - probably going away
+		self.__objects = {}
+		
+		#
+		# alternate experiment
+		#  - Console can open one "subconsole" - Typically a Wrap object
+		#    that calls some object's methods as directed by a line of
+		#    input.
+		#
+		self.__wrappers = config.get('wrappers', {})
+		self.__object = None
+		
+		
+		#
+		# PLUGINS
+		#
+		
+		# plugin config
 		pconf = config.get('plugins', {})
 		self.__plugins = {}
 		
-		# exerimental
-		self.__objects = {}
-		
+		# plugin load
 		perrors = {}
 		for p in pconf:
 			try:
@@ -105,43 +124,143 @@ class Console(EncodingHelper):
 		if perrors:
 			raise Exception("plugin-load-errors", xdata(errors=perrors))
 		
-		# for display of text
+		# LINES - for display of text
 		self.__lines = Lines()
 		
-		# prompt active
+		# ACTIVE - Is the prompt active? If not, exit this console.
 		self.__active = False
 	
 	
 	
 	@property
+	def ident(self):
+		"""
+		An informative (and hopefully unique) identity string that's
+		used as the prompt for a given console.
+		
+		The returned string is "{username}@{modulename}/{Identity}",
+		where the suggested Identity is a classname, or a more
+		descriptive term, if appropriate. These values are generated
+		in the constructor, but subclasses can easily alter the
+		id_suffix property to return a more suitable Identity if
+		necessary.
+		"""
+		return "%s/%s" % (self.id_prefix, self.id_suffix)
+	
+	@property
+	def id_prefix(self):
+		"""
+		Returns a format string that merges the system username, the
+		module name, and a distishing, meaningful name of any kind -
+		typically a class name, but potentially some other name 
+		representative of the function the prompt intends to perform.
+		"""
+		return self.__ident_prefix
+	
+	@property
+	def id_suffix(self):
+		"""
+		Ident line suffix is the class name, is the last element in the
+		construction of the prompt string.
+		
+		Wrappers should override this property, setting it to the wrapped
+		class's name. Subclasses should use the class name of the object
+		they're "prompting" for, unless a different name seems more 
+		appropriate or descriptive of the its function.
+		"""
+		return type(self).__name__
+	
+	@property
+	def prompt(self):
+		"""
+		The command-line input prompt for this object.
+		"""
+		return "%s: " % self.ident
+	
+	@property
 	def lines(self):
+		"""
+		Convenience. A fmt.Lines object used to format lines of text
+		to within a preconfigured width.
+		"""
 		return self.__lines	
 	
 	@property
 	def title(self):
+		"""This console's title string, as read from config."""
 		return self.__title	
 	
 	@property
 	def about(self):
+		"""This console's about section text, as read from config."""
 		return self.__about	
 	
 	@property
 	def help(self):
+		"""
+		A dict containing this console's help messages, as read from 
+		config.
+		"""
 		return self.__help	
 	
 	@property
 	def closing(self):
+		"""An 'exit' message string, as read from config."""
 		return self.__closing	
 	
 	@property
 	def plugins(self):
+		"""A dict containing this console's plugins."""
 		return self.__plugins	
-	
 	
 	@property
 	def config(self):
+		"""The entire config dict, for reference."""
 		return self.__config
 	
+	@property
+	def wrappers(self):
+		"""A dict of wrappers available for use by this console."""
+		return self.__wrappers
+	
+	
+	def wrapper(self, wrapperid, *a):
+		"""
+		Return the wrapper from self.wrappers whose key matches
+		`wrapperid`.
+		"""
+		# get the basic config
+		wconf = self.__wrappers[wrapperid]
+		if 'nconfig' in wconf:
+			wconf = Wrapper(trix.nconfig(wconf['nconfig']))
+		elif 'config' in wconf:
+			wconf = Wrapper(trix.config(wconf['config']))
+		else:
+			raise ValueError("Config Requires config or nconfig path.",
+					xdata(config=wconf)
+				)
+		
+		trix.display(wconf),
+		
+		return Wrapper(wconf)
+		
+		# load the wrapper config
+		#wconfig = 
+		
+		"""
+		#
+		# CREATE
+		#  - Use the creation method specified in the config file.
+		#
+		if 'ncreate' in wconf:
+			obj = trix.ncreate(wconf['ncreate'], *a)
+		elif 'create' in wconf:
+			obj = trix.create(wconf['create'], *a)
+		
+		wrap = trix.ncreate("x.wrap.Wrap", obj)
+		self.w = wrap
+		self.o = obj
+		"""
 	
 	#
 	# CONSOLE - Start the console.
@@ -155,163 +274,30 @@ class Console(EncodingHelper):
 			while self.__active:
 				cmd=evt=None
 				try:
-					cmd = xinput("%s: " % self.__prompt)
-					evt = TextEvent(cmd)
-					rsp = self.handle_input(evt)
-					if evt.argc and evt.argv[0]:
-						# Handle_input prints response; just put a blank line
-						# after.
-						print ('')
+					evt = TextEvent(xinput(self.prompt))
+					self.handle_input(evt)
 				
 				except EOFError:
+					# Ctrl-C exits this prompt
 					self.__active = False
 				
 				except Exception as ex:
+					#
+					# Handle other exceptions by displaying them with linedbg
+					#
 					print('') # get off the "input" line
-					linedbg().dbg(self, ex.args, cmd=cmd, evt=evt.dict)
+					event_dict = evt.dict if evt else None
+					linedbg().dbg(self, ex.args, cmd=cmd, evt=event_dict)
 				
 		except KeyboardInterrupt:
-			# print a blank line so exit message won't be on the input line
 			print('')
+		
+		except BaseException:
+			linedbg().dbg(self, "Fatal Exception. %s\n" % self.closing)
+			raise
 		
 		# close banner
 		print("%s\n" % self.closing)
-	
-	
-	
-	#
-	# HANDLE INPUT
-	#  - Override (and add to) these
-	#
-	def handle_input(self, e):
-		"""Handle input event `e`."""
-		
-		if e.argc:
-			
-			# check plugins first
-			for p in self.__plugins:
-				self.__plugins[p].handle(e)
-				if e.reply:
-					self.lines.output ("%s\n" % str(e.reply))
-					return
-			
-			# handle valid commands...
-			if e.argvl[0] == 'help':
-				self.handle_help(e)
-			elif e.argvl[0] == 'plugins':
-				self.lines.output(" ".join(self.plugins.keys()))
-			elif e.argvl[0] in ['jc','conf']:
-				self.handle_config (e)
-			elif e.argvl[0] == 'exit':
-				self.__active = False
-			
-			# if the first argument is blank...
-			elif e.argv[0]:
-				self.handle_error("invalid-command")
-	
-	
-	"""
-	# maybe later
-	def _call_module(self, e):
-		trix.display(e.dict)
-		if e.argvl[0] == 'cq':
-			if e.argc > 1:
-				m = trix.nvalue('data.udata', "query")
-				m(text=e.argv[1][0])
-	"""		
-	
-	
-	#
-	# HANDLERS...
-	#  - Add a handler for each console command you want to implement.
-	#  - Override these if you need to change behavior/output.
-	#
-	def handle_help(self, e):
-		flist = List(sep=': ', titles=sorted(self.__help.keys()))
-		print (flist.format(self.__help))
-	
-	
-	def handle_error(self, err):
-		if err in self.__messages:
-			print ("Error! %s" % self.__messages[err])
-	
-	
-	def handle_config(self, e):
-		
-		# maybe conf is already open...
-		conf = self.__objects.get("config", None)
-		
-		#
-		# REM: argv[0] is the intenal command - the word "config", 
-		#      given in the console
-		#
-		cmd = e.argvl[1]   # the internal command
-		args = e.argvl[2:] # case-insensitive args
-		Args = e.argv[2:]  # case-sensitive args
-		
-		if cmd in ["load","open"]:
-			if conf:
-				# GET THIS INTO DEBUG MESSAGES!
-				self.lines(
-					"""
-					A config file is already open. Enter "config 
-					save", then "config config" before opening a new config
-					file. (If you want to discard unsaved changes, enter 
-					"config reload" before "config close".)
-					"""
-				)
-			else:
-				# Use `Args[0]` (e.argv[2]) since paths are case-sensitive!
-				self.__objects["config"] = JConfig(Args[0], **self.ek)
-		
-		elif not conf:
-			# GET THIS INTO DEBUG MESSAGES!
-			self.lines(
-				"""
-				No config file is open. Enter "config open <PATH>".
-				"""
-			)
-		
-		elif cmd in ["set","add"]:
-			try:
-				# rem Args[0] is the key to set/add/rmv
-				Args[1] = trix.jparse(" ".join(Args[1:]))
-				if cmd == 'set':
-					self.__objects["config"].set(*Args)
-				elif cmd == 'add':
-					self.__objects["config"].add(*Args)
-			except:
-				raise
-		
-		elif cmd == 'rmv':
-			self.__objects["config"].rmv(*Args)
-		
-		elif cmd == 'select':
-			self.__objects["config"].select(*Args)
-		elif cmd == 'deselect':
-			self.__objects["config"].deselect()
-		elif cmd == 'display':
-			self.__objects["config"].display()
-		elif cmd == 'show':
-			self.__objects["config"].show()
-		
-		elif cmd == 'type':
-			self.__objects["config"].type(*Args)
-		elif cmd == 'keys':
-			self.__objects["config"].keys(*Args)
-		elif cmd in ['skeys','selkeys']:
-			self.__objects["config"].keys(*Args)
-		elif cmd == 'path':
-			self.__objects["config"].path
-		
-		elif cmd == 'save':
-			self.__objects["config"].save()
-		
-		else:
-			print ("UNKNOWN COMMAND : %s" % cmd)
-			print (" -- details", trix.display(e.argv))
-		
-	
 	
 	
 	
@@ -330,3 +316,143 @@ class Console(EncodingHelper):
 		print("#")
 	
 	
+	
+	#
+	# HANDLERS...
+	#  - Add a handler for each console command you want to implement.
+	#  - Override these if you need to change behavior/output.
+	#
+	
+	# HANDLE HELP
+	def handle_help(self, e):
+		flist = List(sep=': ', titles=sorted(self.__help.keys()))
+		print (flist.format(self.__help))
+	
+	
+	# HANDLE ERROR
+	def handle_error(self, err, *a):
+		if err in self.__messages:
+			print ("Error! %s" % self.__messages[err])
+		else:
+			print ("Unknown Error!")
+		if a:
+			trix.display({"args":a})
+	
+	
+	# HANDLE INPUT
+	def handle_input(self, e):
+		"""Handle input event `e`."""
+		
+		if e.argc:
+			
+			# check plugins first
+			for p in self.__plugins:
+				self.__plugins[p].handle(e)
+				if e.reply:
+					self.lines.output ("%s\n" % str(e.reply))
+					return
+			
+			# handle valid commands...
+			if e.argvl[0] == 'help':
+				self.handle_help(e)
+			elif e.argvl[0] == 'plugins':
+				self.lines.output(" ".join(self.plugins.keys()))
+			elif e.argvl[0] == 'wrappers':
+				self.lines.output(" ".join(self.wrappers.keys()))
+			elif e.argvl[0] == 'exit':
+				self.__active = False
+			
+			# create and run a Wrapper object's console
+			elif e.argv[0] in self.wrappers:
+				#
+				# Generate a new Wrapper each time; this wrapper object 
+				# holds control until its `console()` method is exited.
+				#
+				
+				# first, get the config out of this command line
+				wrapperid = e.argv[0]
+				wrap_conf = self.wrappers[e.argv[0]]
+				wrap_args = e.argv[1:]
+				
+				# the entire config must be passed
+				w = Wrapper(wrap_conf, *wrap_args)
+				# w.console()
+		
+			#
+			# CHECK FIRST ARG!
+			#  - There's always at least one argument, even if it's ''.
+			#    In this case, nothing (or only white space) was entered,
+			#    so just hit the next line. Otherwise, it's an unknown 
+			#    command, so complain.
+			#
+			elif e.argvl[0]:
+				self.handle_error("unknown-command", *e.argv)
+	
+
+
+
+
+class Wrapper(Console):
+	"""
+	A Wrapper is a Console that holds a "wrapped" python object and 
+	responds to console commands by calling methods of that object.
+	
+	Console creates a Wrapper whenever the first word entered in on
+	line of text matches a key in its `self.wrappers` dict. The key's 
+	value (self.wrappers[key]) is the Wrapper's config.
+	
+	Each wrapper config should contain the following config dict keys: 
+	 * create  - the full pythonic path to the definition of the class
+	             to be wrapped ('create')
+	             ...OR...
+	   ncreate - the inner path to the definition of the class to be 
+	             wrapped ('ncreate').
+	 * args    - a list of args for the object's constructor
+	 * kwargs  - a dict of kwargs for the object's constructor
+	
+	When the Wrapper object's `console()` method is called, commands
+	entered there are parsed and used as follows:
+	 * The first argument (after the command) represents the name
+	   of an attribute in the wrapped object;
+	 * Remaining args are passed to that method and the event's reply
+	   holds the return value.
+	    - NOTE: these args are different from the constructor args;
+	            they're generated from the prompt line and are received
+	            by `Wrapper.handle_input()`.
+	   
+	"""
+	
+	def __init__(self, config, *a, **k):
+		
+		try:
+			#
+			# Load this wrapper's config file
+			#
+			if 'config' in config:
+				conf = trix.config(config['config'])
+			else:
+				conf = trix.nconfig(config['nconfig'])
+			
+			#trix.display (["wrapper config", config])
+			#print('')
+			
+			Console.__init__(self, config, *a, **k).console()
+			#trix.display([2, self.config])
+		except Exception as ex:
+			raise type(ex)(xdata(config=self.config, given=conf, a=a, k=k))
+	
+	def console(self):
+		print ("Hello")
+		pass
+	
+	
+	@property
+	def id_suffix(self):
+		return type(self.__wrap.obj).__name__
+	
+	
+	def handle_input(self, e):
+		r = self.__wrapped(*e.args)
+		print ("r", r)
+		e.reply(r)
+
