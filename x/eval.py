67b0ca4324ea05818bc0142a17570c8ffca9aa9c
justworx <justworx@protonmail.com>
2018-10-21 23:09:56 -0500

    experimental eval method

---
+++ x/eval.py
@@ -0,0 +1,10 @@
+	
+	
+	# EVAL
+	@classmethod
+	def eval(cls, x, **k):
+		"""Evaluate with ast literal eval; fallback on jparse."""
+		try:
+			return cls.module('ast').literal_eval(x)
+		except:
+			return cls.jparse(x)
