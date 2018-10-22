dbe91f46db078b8cd81da91ca4d9ec93fbba87c3
justworx <justworx@protonmail.com>
2018-10-22 01:22:26 -0500

    more piddeling; this is a stupid function anyway. fnmatch.filter already does it, mostly.

--- x/xmatch.py
+++ x/xmatch.py
@@ -8,23 +8,12 @@ import fnmatch
 
 
 class xmatch(object):
-	def __init__(self, pattern):
-		self.pattern = pattern
-	
-	def __call__(self, obj):
-		self.match(obj)
+	"""Like fnmatch, but works on list items and dict keys, too."""
 	
 	@classmethod
-	def match(cls, obj, pattern=None):
-		pattern = pattern or self.pattern
+	def match(cls, obj, pattern, search=None):
+		# list
 		try:
-			# string
-			return fnmatch.fnmatch(obj, pattern)
-		except:
-			pass
-		
-		try:
-			# list
 			r = []
 			for item in obj:
 				if fnmatch.fnmatch(item, pattern):
@@ -33,15 +22,38 @@ class xmatch(object):
 		except:
 			pass
 		
+		# dict
 		try:
-			# dict key
 			r = {}
-			for k in obj:
-				if fnmatch.fnmatch(k, pattern):
-					r[k] = obj[k]
+			if search=='key':
+				for k in obj:
+					if fnmatch.fnmatch(k, pattern):
+						r[k] = obj[k]
+			elif search='value':
+				for k in obj:
+					if fnmatch.fnmatch(obj, pattern):
+						r[k] = obj[k]
+			else:
+				for k in obj:
+					if fnmatch.fnmatch(obj, key):
+						r[k] = obj[k]
+					elif fnmatch.fnmatch(obj, pattern):
 			return r
 		except:
 			pass
 		
+		# string
+		try:
+			return fnmatch.fnmatch(obj, pattern)
+		except:
+			return fnmatch.fnmatch(str(obj), pattern)
+		
 		return None
+	
+	
+	def __init__(self, pattern):
+		self.pattern = pattern
+	
+	def __call__(self, obj):
+		self.match(self.pattern, obj, search=None)
 
