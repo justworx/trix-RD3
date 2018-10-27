19a665be31d9b15538c7652ae78938ce7ae396e5
justworx <justworx@protonmail.com>
2018-10-27 12:38:25 -0500

    add path and cd commands

--- app/plugin/dir.py
+++ app/plugin/dir.py
@@ -14,7 +14,7 @@ class Dir(Plugin):
 	COMMANDS = {
 		'cd'   : lambda d,*a: d.cd(*a),
 		'ls'   : lambda d,*a: d.ls(*a),
-		'mv'   : lambda d,*a: d.mv(*a),
+		'mv'   : lambda d,*a: d.mv(*a)
 	}
 	
 	def __init__(self, pname, owner, config, **k):
@@ -30,8 +30,19 @@ class Dir(Plugin):
 	def handle(self, e):
 	
 		cmd = e.argvl[0]
-		if cmd in self.COMMANDS:
+		if cmd == 'path':
+			self.reply(e, self.__dir.path)
+		
+		elif cmd == 'cd':
+			try:
+				self.__dir.cd(e.argv[1])
+				self.reply(e, self.__dir.path)
+			except Exception as ex:
+				raise type(ex)(xdata(dir=self.__dir, e=e))
+		
+		elif cmd in self.COMMANDS:
 			x = self.COMMANDS[cmd]
+			#print (x)
 			a = e.argv[1:]
 			r = x(self.__dir, *a)
 			if r:
