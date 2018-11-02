4b3e5ebf89bdce5fae8cf97606fbfc01194e915e
justworx <justworx@protonmail.com>
2018-11-02 15:46:49 -0500

    bugfixes; add comments; add offset in prep for potential use by cline

--- util/clargs.py
+++ util/clargs.py
@@ -7,15 +7,14 @@
 import sys
 
 
-class clargs(self, offset=0):
+class clargs(object):
 	"""
 	Parse a line of text to a dict containing args, kwargs, and flags.
 	
 	The trix package uses a specific standard for parsing text 
-	commands. It's different from both *nix and Windows command line
-	formats, but should facilitate both. 
+	commands. It's different from both *nix and Windows command 
+	line formats, but should facilitate both. 
 	
-	It goes like this:
 	 * Anything prefixed with "--" is added to a dict used to provide
 	   keyword arguments to a function or method. The kwarg must be
 	   given in the format "--name=value"
@@ -25,27 +24,36 @@ class clargs(self, offset=0):
 	   individual arguments.
 	
 	NOTE that this is not intended as an all-inclusive method for 
-	     passing arguments. Any args or kwargs must be a single string,
-	     so it's not a straightforward process to pass.
-	     
-	     For example: passing "--list=dog cat mouse" to `clargs` will
-	     result in {"kwargs":{'list':'dog'}, "args":['cat', 'mouse']}  
-	     
-	     This is true for any command line argument (either from a
-	     shell, or via a shell-like system such as `app.console`.
-	     
-	     When complex arguments are needed, the values will may be
-	     compressed to a non-breaking string (eg, base64) before being
-	     passed as an 'arg' or 'kwarg'.
+     passing arguments. Any args or kwargs must be a single string,
+     so it's not a straightforward process to pass.
+     
+     For example: passing "--list=dog cat mouse" to `clargs` will
+     result in {"kwargs":{'list':'dog'}, "args":['cat', 'mouse']}  
+     
+     This is true for any command line argument (either from a
+     shell, or via a shell-like system such as `app.console`.
+     
+     When complex arguments are needed, the values will may be
+     compressed to a non-breaking string (eg, base64) before being
+     passed as an 'arg' or 'kwarg'.
 	"""
 	
-	
-	def __init__(self):
+	def __init__(self, offset=0):
+		"""
+		Pass an offset (default: 0) that marks the beginning of command
+		line items (split on ' ' space) to be parsed. For example, the
+		trix.app.cline class would start at offset 3 to receive arguments
+		['alpha3:', 'A', 'B', 'C'] while ignoring the "python -m trix" 
+		part of the original command line.
+		
+		$ python3 -m trix alpha3: A B C
+		"""
+		
+		self.offset = offset
 		self.args = []
 		self.flags = ''
 		self.kwargs = {}
-	
-	
+		
 	def parse (self, text):
 		self.args = sys.argv[offset]
 		self.flags = ''
@@ -60,7 +68,7 @@ class clargs(self, offset=0):
 			else:
 				self.args.append(a)
 		
-		return dict('args':args, 'kwargs':kwargs, 'flags':flags)
+		return {'args':args, 'kwargs':kwargs, 'flags':flags}
 
 
 
